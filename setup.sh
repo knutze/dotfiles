ab7702ba86e5b0b8825ec1a678cf304cca1e6a5f
knutze <shaktea@gmail.com>
2018-08-03 03:54:16 +0900

    Overhaul, add ripgrep role.

--- setup.sh
+++ setup.sh
@@ -2,20 +2,20 @@
 
 set -xe
 
-if type apt-get; then
+if ! type ansible; then
+  if type apt-get; then
     sudo apt-get update
     sudo apt-get -y install software-properties-common
     sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt-get update
     sudo apt-get -y install ansible
-elif type yum; then
+  elif type yum; then
     sudo yum clean all
     sudo yum install -y ansible
+  fi
 fi
 
-ansible-playbook -i localhost, -v setup.yml
-
-exec ${SHELL}
+ansible-playbook -i hosts setup.yml -K
 
 vim +PlugInstall +qall
 
