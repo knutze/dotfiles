b8f1a5bed25cf94021d364034d63f9d32e94655e
knutze <shaktea@gmail.com>
2018-08-10 01:43:37 +0900

    Add pyenv role.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-set -xe
+(set -e
 
-if ! type ansible; then
-  if type apt-get; then
-    sudo apt-get update
-    sudo apt-get -y install software-properties-common
-    sudo apt-add-repository -y ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt-get -y install ansible
-  elif type yum; then
-    sudo yum clean all
-    sudo yum install -y ansible
+  if ! type ansible; then
+    if type apt-get; then
+      sudo apt-get update
+      sudo apt-get -y install software-properties-common
+      sudo apt-add-repository -y ppa:ansible/ansible
+      sudo apt-get update
+      sudo apt-get -y install ansible
+    elif type yum; then
+      sudo yum clean all
+      sudo yum install -y ansible
+    fi
   fi
-fi
 
-ansible-playbook -i hosts setup.yml -K
+  ansible-playbook -i hosts setup.yml -K
 
-vim +PlugInstall +qall
+  vim +PlugInstall +qall
+
+) && exec $SHELL
