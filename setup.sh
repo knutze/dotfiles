57953eb5d2cbefa0c52ea3f75a94ac743a7bbe82
knutze <shaktea@gmail.com>
2018-11-16 13:24:44 +0900

    Fix for proxy environment and raspbian.

--- setup.sh
+++ setup.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 
-(set -e
+sudo='sudo -E'
 
-  if ! type ansible; then
-    if type apt-get; then
-      sudo apt-get update
-      sudo apt-get -y install software-properties-common
-      sudo apt-add-repository -y ppa:ansible/ansible
-      sudo apt-get update
-      sudo apt-get -y install ansible
-    elif type yum; then
-      sudo yum clean all
-      sudo yum install -y ansible
-    fi
+if ! type ansible; then
+  if type apt-get; then
+    $sudo apt-get update
+    $sudo apt-get -y install software-properties-common
+    $sudo apt-add-repository -y ppa:ansible/ansible
+    $sudo apt-get update
+    $sudo apt-get -y install ansible
+  elif type yum; then
+    $sudo yum clean all
+    $sudo yum install -y ansible
   fi
+fi
 
-  ansible-playbook -i hosts setup.yml -K
-
-  vim +PlugInstall +qall
-
-) && source ~/.bashrc
+ansible-playbook -i hosts setup.yml -K \
+  && vim +PlugInstall +qall \
+  && source ~/.bashrc
