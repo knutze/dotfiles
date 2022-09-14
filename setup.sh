b454f7bf80ec5569eb7e96203632e2945395cf85
knutze <shaktea@gmail.com>
2022-09-15 04:33:52 +0900

    Update for MSYS2 environment

--- setup.sh
+++ setup.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
 sudo='sudo -E'
 
 if ! type ansible; then
-  if type apt-get; then
-    $sudo apt-get update
-    $sudo apt-get -y install software-properties-common
-    $sudo apt-add-repository -y ppa:ansible/ansible
-    $sudo apt-get update
-    $sudo apt-get -y install ansible
-  elif type yum; then
-    $sudo yum clean all
-    $sudo yum install -y ansible
-  fi
+
+if type pacman; then
+  pacman -S ansible
+elif type apt-get; then
+  $sudo apt-get update
+  $sudo apt-get -y install software-properties-common
+  $sudo apt-add-repository -y ppa:ansible/ansible
+  $sudo apt-get update
+  $sudo apt-get -y install ansible
+elif type yum; then
+  $sudo yum clean all
+  $sudo yum install -y ansible
 fi
 
-ansible-playbook -i hosts setup.yml -K \
-  && vim +PlugInstall +qall \
-  && source ~/.bashrc
+fi
+
+ansible-playbook -i hosts setup.yml
+source ~/.bashrc
